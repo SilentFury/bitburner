8f6fbef5f2c7d7c5c1c4e6cdb2b239fc9560d9da
George Nik <geor.nik32@gmail.com>
2021-12-18 01:59:58 +0200

    Update grow.js

--- grow.js
+++ grow.js
@@ -1,12 +1,22 @@
 /** @param {NS} ns **/
 export async function main(ns) {
 	ns.disableLog ("ALL");
+	if (ns.args.length == 0) {
+		ns.tprint ("Error: No target argument set, must be a valid hostname.");
+		ns.exit ();
+	}
 	var hostname = ns.args[0];
+	var bankAmount = ns.getServerMoneyAvailable (hostname)
 	var out;
-	ns.print ("Begin automated bank spoofing process of [" + hostname + "]");
-	ns.print ("ETA: " + ns.tFormat(ns.getGrowTime(hostname)));
+
+	ns.print ("~~~~~~~~~~~~~~~~~~~");
+	ns.print ("Begin bank spoofing process of [" + hostname + "]");
+	ns.print ("Cash: " + ns.nFormat(bankAmount, "0,0.00 a") + 
+		" | ETA: " + ns.tFormat(ns.getGrowTime (hostname)));
 	while (true) {
 		out = await ns.grow (hostname);
-		ns.print ("Host bank has been spoofed for " + ns.nFormat(out, "0,0.000") + "%!");
+		bankAmount = ns.getServerMoneyAvailable (hostname);
+		ns.print ("Target bank modified by " + ns.nFormat (out, "0,0.000") + "!");
+		ns.print ("Cash: " + ns.nFormat(bankAmount, "0,0.00 a"));
 	}
 }
