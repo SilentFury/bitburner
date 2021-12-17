5cef838c5876155b6c4d2457bd4e3e9295164a1a
George Nik <geor.nik32@gmail.com>
2021-12-17 17:32:15 +0200

    Update start.js

--- start.js
+++ start.js
@@ -1,19 +1,20 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-    var source = "https://github.com/SilentFury/bitburner/blob/02c991e63e8e8533ac087d00f7195f8f6491097b/";
+    var url = "https://raw.githubusercontent.com/SilentFury/bitburner/main/";
     var files = ["hack.js", "break.js", "grow.js", "weaken.js", 
                 "buyServer.js", "rmServer.js", "botExec.js", "botUpload.js"];
-    var out, i, count = 0;
+    var out, i, source, count = 0;
     ns.tprint ("Downloading necessary scripts...");
     for (i=0; i<files.length; i++) {
-        out = await ns.wget (source + "" + files[i], files[i], "home");
+        source = url + "" + files[i];
+        out = await ns.wget (source, files[i], "home");
         if (out) {
             ns.tprint ("> " + files[i] + " downloaded");
             count++;
         }else{
             ns.tprint ("> failed to get " + files[i]);
         }
-        ns.sleep (1000);
+        await ns.sleep (800);
     }
-    ns.tprint ("Download finished! " + count + "out of " + files.length + " files successfully downloaded.");
+    ns.tprint ("Download finished! " + count + " out of " + files.length + " files successfully downloaded.");
 }
