a9d95b28de444c0855f0ab5369da24a02d8ad5c9
George Nik <geor.nik32@gmail.com>
2021-12-18 02:09:05 +0200

    Update weaken.js

--- weaken.js
+++ weaken.js
@@ -1,12 +1,22 @@
 /** @param {NS} ns **/
 export async function main(ns) {
 	ns.disableLog ("ALL");
+	if (ns.args.length == 0) {
+		ns.tprint ("Error: Missing arguments, must be a valid hostname.");
+		ns.exit ();
+	}
 	var hostname = ns.args[0];
+	var secLevel = ns.getServerSecurityLevel (hostname);
 	var out;
-	ns.print ("Begin automated weakening process of [" + hostname + "]");
-	ns.print ("ETA: " + ns.tFormat(ns.getGrowTime(hostname)));
+
+	ns.print ("~~~~~~~~~~~~~~~~~~~");
+	ns.print ("Begin weakening process of [" + hostname + "]");
+	ns.print ("Security: " + ns.nFormat (secLevel, "0,0.00") + 
+		" | ETA: " + ns.tFormat(ns.getGrowTime(hostname)));
 	while (true) {
 		out = await ns.weaken (hostname);
-		ns.print ("Host security has been modified by " + ns.nFormat(out, "0,0.00") + "!");
+		secLevel = ns.getServerSecurityLevel (hostname);
+		ns.print ("Target security modified by " + ns.nFormat (out, "0,0.00") + "!"); 
+		ns.print ("Security: " + ns.nFormat (secLevel, "0,0.00"));
 	}
 }
