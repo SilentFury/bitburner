3dbb2b24982286571229e30ea25deebf6aa49fbe
George Nik <geor.nik32@gmail.com>
2021-12-18 02:05:32 +0200

    Update buyServer.js

--- buyServer.js
+++ buyServer.js
@@ -1,14 +1,15 @@
 /** @param {NS} ns **/
 export async function main(ns) {
+	if (ns.args.length == 0) {
+		ns.tprint ("Warning: Missing arguments, must include valid hostname and RAM amount.");
+		ns.exit ();
+	}
 	var servers, maxServers, hostname, maxRAM, check;
 	servers = ns.getPurchasedServers().length;
 	maxServers = ns.getPurchasedServerLimit()
 	hostname = ns.args[0];
 	maxRAM = ns.args[1];
-	if (hostname == "" || maxRAM == "") {
-		ns.tprint ("Warning: Missing arguments, please include hostname and RAM amount.");
-		ns.exit ();
-	}
+
 	ns.tprint ("Attempting to buy server [" + hostname + "] with " + maxRAM + " RAM");
 	ns.tprint ("Current servers: " + servers + " / " + maxServers);
 	ns.tprint ("Server cost: $" + ns.getPurchasedServerCost(maxRAM));
