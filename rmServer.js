3f16225299e82f7de2d48e4be2f8e9d2051d37ab
George Nik <geor.nik32@gmail.com>
2021-12-18 02:04:06 +0200

    Update rmServer.js

--- rmServer.js
+++ rmServer.js
@@ -1,8 +1,12 @@
 /** @param {NS} ns **/
 export async function main(ns) {
     var check = false;
+    if (ns.args.length == 0) {
+	    ns.tprint ("Warning: No hostname argument, must include valid hostname.");
+	    ns.exit ();
+	}
     var hostname = ns.args[0];
     check = ns.deleteServer(hostname);
-    if (check) ns.tprint ("Success! Server " + hostname + " has been removed!");
-    else ns.tprint ("Error! Server " + hostname + " could not be removed!");
+    if (check) ns.tprint ("Server " + hostname + " has been removed.");
+    else ns.tprint ("Error: Server " + hostname + " could not be removed.");
 }
