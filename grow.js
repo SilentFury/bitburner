2ab2157db4e0f9566f91205465ec98c32bcb5a51
George Nik <geor.nik32@gmail.com>
2024-04-18 01:53:34 +0300

    Switched nFormat to formatNumber and added threshold control

--- grow.js
+++ grow.js
@@ -1,22 +1,29 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	ns.disableLog ("ALL");
-	if (ns.args.length == 0) {
-		ns.tprint ("Error: Missing arguments, must be a valid hostname.");
-		ns.exit ();
-	}
-	var hostname = ns.args[0];
-	var bankAmount = ns.getServerMoneyAvailable (hostname)
-	var out;
+  ns.disableLog ("ALL");
+  if (ns.args.length == 0) {
+    ns.tprint ("Error: Missing arguments, must be a valid hostname.");
+    ns.exit ();
+  }
 
-	ns.print ("~~~~~~~~~~~~~~~~~~~");
-	ns.print ("Begin bank spoofing process of [" + hostname + "]");
-	ns.print ("Cash: " + ns.nFormat(bankAmount, "0,0.00 a") + 
-		" | ETA: " + ns.tFormat(ns.getGrowTime (hostname)));
-	while (true) {
-		out = await ns.grow (hostname);
-		bankAmount = ns.getServerMoneyAvailable (hostname);
-		ns.print ("Target bank modified by " + ns.nFormat (out, "0,0.000") + "!");
-		ns.print ("Cash: " + ns.nFormat(bankAmount, "0,0.00 a"));
-	}
+  var hostname = ns.args[0];
+  const moneyThresh = ns.getServerMaxMoney (hostname) * 0.75;
+  var bankAmount = ns.getServerMoneyAvailable (hostname)
+  var out;
+
+  ns.print ("~~~~~~~~~~~~~~~~~~~");
+  ns.print ("Begin bank spoofing process of [" + hostname + "]");
+  ns.print ("Cash: $" + ns.formatNumber(bankAmount, 3, 1000, false) + 
+    " | ETA: " + ns.tFormat(ns.getGrowTime (hostname)));
+
+  while (true) {
+    if (ns.getServerMoneyAvailable (hostname) < moneyThresh) {
+      out = await ns.grow (hostname);
+      ns.print ("Target bank modified by $" + ns.formatNumber (out, 3, 1000, false) + "!");
+    } else {
+      sleep (5000);
+    }
+    bankAmount = ns.getServerMoneyAvailable (hostname);
+    ns.print ("Cash: $" + ns.formatNumber (bankAmount, 3, 1000, false));
+  }
 }
