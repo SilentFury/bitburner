53f7601dc675f6585722deb594ce77dea0281b94
George Nik <geor.nik32@gmail.com>
2024-04-18 01:57:50 +0300

    Switched nFormat to formatNumber

--- weaken.js
+++ weaken.js
@@ -1,22 +1,29 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	ns.disableLog ("ALL");
-	if (ns.args.length == 0) {
-		ns.tprint ("Error: Missing arguments, must be a valid hostname.");
-		ns.exit ();
-	}
-	var hostname = ns.args[0];
-	var secLevel = ns.getServerSecurityLevel (hostname);
-	var out;
+  ns.disableLog ("ALL");
+  if (ns.args.length == 0) {
+    ns.tprint ("Error: Missing arguments, must be a valid hostname.");
+    ns.exit ();
+  }
 
-	ns.print ("~~~~~~~~~~~~~~~~~~~");
-	ns.print ("Begin weakening process of [" + hostname + "]");
-	ns.print ("Security: " + ns.nFormat (secLevel, "0,0.00") + 
-		" | ETA: " + ns.tFormat(ns.getGrowTime(hostname)));
-	while (true) {
-		out = await ns.weaken (hostname);
-		secLevel = ns.getServerSecurityLevel (hostname);
-		ns.print ("Target security modified by " + ns.nFormat (out, "0,0.00") + "!"); 
-		ns.print ("Security: " + ns.nFormat (secLevel, "0,0.00"));
-	}
+  var hostname = ns.args[0];
+  const secThresh = ns.getServerMinSecurityLevel (hostname);
+  var secLevel = ns.getServerSecurityLevel (hostname);
+  var out;
+
+  ns.print ("~~~~~~~~~~~~~~~~~~~");
+  ns.print ("Begin weakening process of [" + hostname + "]");
+  ns.print ("Security: " + ns.formatNumber (secLevel, 3, 1000, false) + 
+    " | ETA: " + ns.tFormat(ns.getGrowTime(hostname)));
+
+  while (true) {
+    if (ns.getServerSecurityLevel (hostname) > secThresh) {
+      out = await ns.weaken (hostname);
+      ns.print ("Target security modified by " + ns.formatNumber (out, 3, 1000, false) + "!"); 
+    } else {
+      sleep (5000);
+    }
+    secLevel = ns.getServerSecurityLevel (hostname);
+    ns.print ("Security: " + ns.formatNumber (secLevel, 3, 1000, false));
+  }
 }
