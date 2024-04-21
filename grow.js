9343ed12f78c8fc7510664503a6f3f20e0fbbaaa
George Nik <geor.nik32@gmail.com>
2024-04-21 13:30:16 +0300

    Updated grow.js to match hack.js current changes

--- grow.js
+++ grow.js
@@ -7,23 +7,25 @@ export async function main(ns) {
   }
 
   var hostname = ns.args[0];
-  const moneyThresh = ns.getServerMaxMoney (hostname) * 0.75;
+  const bankThresh = ns.getServerMaxMoney (hostname) * 0.75;
   var bankAmount = ns.getServerMoneyAvailable (hostname)
   var out;
 
   ns.print ("~~~~~~~~~~~~~~~~~~~");
   ns.print ("Begin bank spoofing process of [" + hostname + "]");
-  ns.print ("Cash: $" + ns.formatNumber(bankAmount, 2, 1000, false) + 
+  ns.print ("Max Cash: " + ns.formatNumber (ns.getServerMaxMoney(hostname), 2, 1000, false) 
+    + " | Threshold: " + ns.formatNumber (bankThresh, 2, 1000, false));
+  ns.print ("Current cash: $" + ns.formatNumber(bankAmount, 2, 1000, false) + 
     " | ETA: " + ns.tFormat(ns.getGrowTime (hostname)));
-
   while (true) {
-    if (ns.getServerMoneyAvailable (hostname) < moneyThresh) {
+    if (bankAmount < bankThresh) {
       out = await ns.grow (hostname);
-      ns.print ("Target bank modified by " + ns.formatPercent (out, 4, 100, false) + "%!");
+      ns.print ("Target bank modified by x" + ns.formatNumber (out, 4, 1000, false) + "!");
     } else {
-      sleep (5000);
+      await sleep (5000);
     }
     bankAmount = ns.getServerMoneyAvailable (hostname);
-    ns.print ("Cash: $" + ns.formatNumber (bankAmount, 2, 1000, false));
+		ns.print ("Current cash: $" + ns.formatNumber(bankAmount, 2, 1000, false) + 
+      " | ETA: " + ns.tFormat(ns.getGrowTime (hostname)));
   }
 }
