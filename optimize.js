17f75b592619648f4702d0234d23752efe19fc3a
George Nik <geor.nik32@gmail.com>
2024-04-21 22:23:10 +0300

    Removed verbose, added ram percentage option

--- optimize.js
+++ optimize.js
@@ -1,42 +1,44 @@
 /** @param {NS} ns **/
 export async function main(ns) {
 
-	var hostname = ns.getHostname ();
-	var script = ns.args[0] || "";
-	var target = ns.args[1] || "";
-	if (script == "" || target == "") {
-		if (script == "") ns.tprint ("Error: Argument [0] is empty, must be a valid script name.");
-		if (target == "") ns.tprint ("Error: Argument [1] is empty, must be a valid server hostname.");
-		ns.exit ();
-	}
-	if (!ns.fileExists (script, hostname)) {
-		ns.tprint ("Error: Script [" + script + "] could not be found.");
-		ns.exit ();
-	}
-	if (!ns.serverExists (target)) {
-		ns.tprint ("Error: Server [" + target + "] does not exist.");
-		ns.exit ();
-	}
-	var scriptRAM = ns.getScriptRam (script, hostname);
-	var usedRAM = ns.getServerUsedRam (hostname) - ns.getScriptRam (ns.getScriptName(), hostname);
-	var maxRAM = ns.getServerMaxRam (hostname);
-	var availRAM = maxRAM - usedRAM;
-	var numThreads;
+  var localhost = ns.getHostname ()
+  var script = ns.args[0]
+  var target = ns.args[1]
+  var ramPerc = ns.args[2] || 100
 
-	var verbose = ns.args[2];
+  if (ns.args[0].length == 0) {
+    ns.tprint ("Error: Argument [0] (script) is empty, must be a valid script name or 'help'.")
+    ns.exit
+  } else if (ns.args[0] == "help") {
+    ns.tprint ("> optimize.js syntax")
+    ns.tprint ("optimize.js (script|help) hostname ram_percentage")
+    ns.tprint ("ram_percentage defaults to 100, must be an integer within 1 - 100")
+    ns.exit
+  }
+  if (ns.args[1].length == 0) {
+    ns.tprint ("Error: Argument [1] (target) is empty, must be a valid server hostname.")
+    ns.exit
+  }
+  if (ramPerc <= 0 || ramPerc > 100) {
+    ns.tprint ("Error: Argument [2] (ram_percentage) needs to be an integer between 1 and 100")
+    ns.exit
+  }
+  if (!ns.fileExists (script, localhost)) {
+    ns.tprint ("Error: Script [" + script + "] could not be found on [" + localhost + "].")
+    ns.exit ()
+  }
+  if (!ns.serverExists (target)) {
+    ns.tprint ("Error: Target host [" + target + "] does not exist.")
+    ns.exit ()
+  }
+  
+  const scriptRAM = ns.getScriptRam (script, localhost)
+  const usedRAM = ns.getServerUsedRam (localhost) - ns.getScriptRam (ns.getScriptName(), localhost)
+  const maxRAM = ns.getServerMaxRam (localhost)
+  var availRAM = (maxRAM - usedRAM)*(ramPerc/100)
+  var numThreads
 
-	if (verbose) {
-		ns.tprint ("~~~~~~~~~~~~~~~~~~~~~");
-		ns.tprint ("Hack script optimizer");
-		ns.tprint ("~~~~~~~~~~~~~~~~~~~~~");
-		ns.tprint ("Host: " + hostname + " | Program: " + script + " (" + scriptRAM + " GB)" + " | Target: " + target);
-		ns.tprint ("Max RAM: " + maxRAM + " | Used: " + usedRAM + " | Available: " + availRAM);
-		ns.tprint ("~~~~~~~~~~~~~~~~~~~~~");
-	}
-	numThreads = Math.floor (availRAM/scriptRAM);
-	if (verbose) {
-		ns.tprint ("Launching " + script + " with " + numThreads + " threads");
-		ns.toast ("Launching " + script + " with " + numThreads + " threads");
-	}
-	ns.spawn (script, numThreads, target);
+  numThreads = Math.floor (availRAM/scriptRAM)
+  ns.tprint ("Launching " + script + " on " + localhost + " against " + target + " using " + numThreads + " threads")
+  ns.spawn (script, numThreads, target)
 }
